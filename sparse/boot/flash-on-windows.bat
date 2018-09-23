2ddc528bb97c1f75c6cad797d643ef95e4c51af7
Björn Bidar <theodorstormgrade@gmail.com>
2018-09-24 00:00:58 +0200

    [flashing] Support the check for product on Windows. Fixes MER#1961

--- sparse/boot/flash-on-windows.bat
+++ sparse/boot/flash-on-windows.bat
@@ -31,7 +31,7 @@ call :sleep 3
 @call :md5sum flash-on-windows.bat
 
 set fastbootcmd_no_device=fastboot.exe
-set current_device=
+set products=
 
 echo(
 echo Searching a compatible device...
@@ -58,20 +58,16 @@ exit /b 1
 
 :no_error_serialnumbers
 
+for %%d in ( %serialnumbers% ) do (
+  set fastbootcmd=%fastbootcmd_no_devic% -s %%d
+  @call :getvar product
+  findstr /R /C:"product: h4113" %tmpflashfile% >NUL 2>NUL
+  if not errorlevel 1 (
+    call :new_product_found %%d
+  )
+)
 
-if "%serialnumbers%" == "%serialnumbers: =%" GOTO no_multiple_serialnumbers
-
-echo(
-echo It seems that there are multiple compatible devices connected in fastboot mode.
-echo Make sure only the device that you intend to flash is connected.
-pause
-exit /b 1
-
-:no_multiple_serialnumbers
-
-set current_device=%serialnumbers%
-
-if not [%current_device%] == [] GOTO no_error_product
+if not [%products] == [] goto no_error_product 
 
 echo(
 echo The DEVICE this flashing script is meant for WAS NOT FOUND!
@@ -83,8 +79,19 @@ exit /b 1
 
 :no_error_product
 
+if "%products" == "%products: =%" GOTO no_multiple_products
+
+echo(
+echo It seems that there are multiple compatible devices connected in fastboot mode.
+echo Make sure only the device that you intend to flash is connected.
+pause
+exit /b 1
+
+
+:no_multiple_products
+
 :: Now we know which device we need to flash
-set fastbootcmd=%fastbootcmd_no_device% -s %current_device%
+set fastbootcmd=%fastbootcmd_no_device% -s %products%
 
 :: Check that device has been unlocked
 @call :getvar secure
@@ -178,6 +185,15 @@ if "%serialnumbers%" == "" (
 set serialnumbers=%serialno%
 ) else (
 set "serialnumbers=%serialno% %serialnumbers%"
+)
+@exit /b 0
+
+:new_product_found
+set product=%1
+if "%products" == "" (
+set products=%product%
+) else (
+set "products=%product %products"
 )
 @exit /b 0
 
